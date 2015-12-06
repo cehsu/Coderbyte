a60a466c0d793ba426564b4dbd07fa1fcbb6de1a
cehsu <clairehsu86@gmail.com>
2015-12-06 15:47:14 -0800

    Create 27-PowersofTwo.js

---
+++ 27-PowersofTwo.js
@@ -0,0 +1,13 @@
+function PowersofTwo(num) { 
+while (num > 0 ) {
+  if (num == 1) return true;
+  else return PowersofTwo(num/2);
+}
+  // code goes here  
+  return false; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+PowersofTwo(readline());           
