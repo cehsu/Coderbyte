58a51902a85cf22b26e31f225127ab02f82d780a
cehsu <clairehsu86@gmail.com>
2015-12-06 15:36:05 -0800

    Create 08-CheckNums.js

---
+++ 08-CheckNums.js
@@ -0,0 +1,11 @@
+function CheckNums(num1,num2) { 
+
+  // code goes here  
+  if (num1 == num2) return -1;
+else   return num1 < num2; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+CheckNums(readline());           
