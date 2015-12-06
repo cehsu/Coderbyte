f869df72533234c1d67ee270398b4c67fc4199de
cehsu <clairehsu86@gmail.com>
2015-12-06 15:37:10 -0800

    Create 10-AlphabetSoup.js

---
+++ 10-AlphabetSoup.js
@@ -0,0 +1,10 @@
+function AlphabetSoup(str) { 
+
+  // code goes here  
+  return str.split("").sort().join(""); 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+AlphabetSoup(readline());           
